58b94a8b48d7d3ef1252550a55081be6bbcdf9db
ExpandingDev <javscripter@yahoo.com>
2020-05-17 20:45:41 -0400

    Added main loop

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import time
+import roverio
+import laserio
+import debug
+import spectrometerio
+
+
+def main_loop():
+	"""This will be the main loop that checks for and processes commands"""
+	print("Main loop entered")
+
+main_loop()
