91b166cd89c348c5547a89d367a48af8895f20df
ExpandingDev <javscripter@yahoo.com>
2020-05-20 11:39:33 -0400

    added attempts to process commands

---
+++ practice/process_command_stream.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Command():
+	id = 0x00
+	name = "COMMAND"
+	def execute(self):
+		print("This is the default command class!")
+		return 0
+
+class InvalidCommand():
+	id = 0xFF
+	name = "INVALID"
+	def execute(self):
+		print("Invalid command received!")
+		return 0
+
+class PingCommand():
+	id = 0x01
+	name = "PING"
+	def execute(self):
+		print("PONG")
+		return 0
+
+class TimeSync():
+	id = 0x0E
+	name = "CLOCK_SYNC"
+	def execute(self):
+
+	def __init__(self, stream):
+
+def main():
+	print("Enter command stream:")
+	s = input()
+	for c in s:
+		if c == "p":
+			p = PingCommand()
+			p.execute()
+
+main()
