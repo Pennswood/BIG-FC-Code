a9858921a451af94881999b6a33edb1a224a387d
nschaffin <53974684+nschaffin@users.noreply.github.com>
2020-05-20 16:44:47 -0400

    Fixed indentation errors

--- status_request.py
+++ status_request.py
@@ -6,9 +6,9 @@ def status_request(self):				# Is file a global var?
 	self.sendBytes(self.laser_status)		# Laser status | 1 byte
 	self.sendBytes(self.spec_status)		# Spectrometer status | 1 byte
 	for i in temp_data:				# Assuming that temp_data is an array
-        self.sendBytes(i)			        # Temperature data | 56 bytes
+        	self.sendBytes(i)			# Temperature data | 56 bytes
 	for i in etch_foil_duty_cycle:	        	# Assuming EFDC is an array
-        self.sendBytes(i)			        # Etch foil duty cycle | 3 bytes
+        	self.sendBytes(i)			# Etch foil duty cycle | 3 bytes
 	self.sendString(self.error_codes)		# Error codes | 3 bytes
 	self.sendBytes(self.cmds_recieved)		# Commands received | 1 byte
 	return None
