5cc3bf89a21ab9357489a4ff856e7febe19ac872
nschaffin <53974684+nschaffin@users.noreply.github.com>
2020-05-20 16:41:41 -0400

    Adding status request file
    This will most likely be subject to change.

---
+++ status_request.py
@@ -0,0 +1,15 @@
+"""
+Sends over the current status of the Oasis (temperature, error codes, laser and spectrometer status)
+ser is the global variable that refers to the rover serial port
+"""
+def status_request(self):				    # Is file a global var?
+	self.sendBytes(self.tlc_mode)			# TLC mode | 1 byte
+	self.sendBytes(self.laser_status)		# Laser status | 1 byte
+	self.sendBytes(self.spec_status)		# Spectrometer status | 1 byte
+	for i in temp_data:				        # Assuming that temp_data is an array
+        self.sendBytes(i)			        # Temperature data | 56 bytes
+	for i in etch_foil_duty_cycle:	        # Assuming EFDC is an array
+        self.sendBytes(i)			        # Etch foil duty cycle | 3 bytes
+	self.sendString(self.error_codes)		# Error codes | 3 bytes
+	self.sendBytes(self.cmds_recieved)		# Commands received | 1 byte
+	return None
