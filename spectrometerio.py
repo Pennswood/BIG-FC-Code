bc0a09b76ce3fda19c7b61e6e45a36cf2bd80fc8
nschaffin <53974684+nschaffin@users.noreply.github.com>
2020-05-29 19:44:15 -0400

    Updating sample function to save data to sdcard

--- spectrometerio.py
+++ spectrometerio.py
@@ -1,4 +1,8 @@
 import seabreeze
+import sdcardio as sdcard
+import subprocess
+import time
+import oasis_serial
 
 '''
 Set up the spectrometer
@@ -16,14 +20,25 @@ class Spectrometer():
     Taking integration time int and spectrometer object as parameters
     """
     def sample(self, milliseconds, spec):
-        spec.trigger_mode = 0                               # Setting the trigger mode to normal
-        spec.integration_time_micros(milliseconds*1000)     # Set integration time for spectrometer
-        self.states_spectrometer = 1
-        #TODO: Send nominal response to rover as specified in rover commands
-        wavelengths, intensities = spec.spectrum()          # This will return wavelengths and intensities as a 2D array
-        #TODO: Save file to rover as specified in rover commands
-        self.states_spectrometer = 0
-        return wavelengths, intensities                     # Returns array for wavelengths and intensities
+        spec.trigger_mode = 0                                                                   # Setting the trigger mode to normal
+        spec.integration_time_micros(milliseconds*1000)                                         # Set integration time for spectrometer
+        self.states_spectrometer = 1                                                            # Spectrometer state is set to sampling
+        oasis_serial.sendBytes('\x01')                                                          # Sending nominal responce
+        try:
+            wavelengths, intensities = spec.spectrum()                                          # This will return wavelengths and intensities as a 2D array, this call also begins the sampling
+        except:
+            return 'An error occured while attempting to sample'                                # Command to sample didn't work properly
+
+        data = wavelengths, intensities                                                         # Saving 2D array to variable data
+        if data == []:
+            return 'No data entered'                                                            # Error handling for no data collected
+        oasis_serial.sendBytes('\x31')                                                          # Code sent to spectrometer signaling sampling has successfully finished
+        date = subprocess.run(['date', "'+%m_%d_%Y_%H_%M_%S'"], timeout = 5, stdout=subprocess.PIPE)    # Running date command on terminal
+        filename = '{}'.format(date.stdout.decode('utf-8'))                                     # Creates the time stamped spectrometer file name
+        seconds = time.time()                                                                   # Returns # of seconds since Jan 1, 1970 (since epoch)
+        sdcard.create_spectrometer_file(filename + '.bin', data, seconds)                       # Function call to create spectrometer file
+        self.states_spectrometer = 0                                                            # Spectrometer state is now on standby
+        return None
 
     def __init__(self):
         # 0 = standby, 1 = integrating, 2 = disconnected
