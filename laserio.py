2ec29755b794a0a3a88e33b6b4623afe57a0b358
ksweens66 <ksweens66@gmail.com>
2020-06-01 16:36:28 -0400

    added GPIO functions & Adafruit library. Commented a few things for clarity.

--- laserio.py
+++ laserio.py
@@ -1,5 +1,6 @@
 from science import laser_control
 import time
+import Adafruit_BBIO.GPIO as GPIO       # Adafruit library for safe GPIO control
 """
 Task: turn TLC to operations temperature, followed by turning on laser.
         This function can be expected to be continuously called until laser is warmed up.
@@ -9,26 +10,29 @@ Output: Integer. 0 = laser warmed up, 1 = laser warming up, 2 = TLC warming modu
 
 
 class Laser():
+    # Laser is powered-up and DISARMED. While warming up, status is 1. When warmed up, status is 2.
     def warm_up_laser(self):
-        #TODO: we don't have correct GPIO pins yet for 48 VDc converter
         status = self.get_status()
         self.oasis_serial(b'\x01')
-        if self.states_laser < 1: #If the laser is already warmed up + armed, it can still run this command and not change anything
+        if self.states_laser < 1: # If the laser is already warmed up + armed, it can still run this command and not change anything
             self.states_laser = 1
 
         while status < 1: # We don't have a signal yet
             status = self.get_status()
         self.oasis_serial(b'\x23') # TODO: Fix from command.
         self.states_laser = 2
+
+        GPIO.output("P9_42", GPIO.HIGH)     # Set pin HIGH to enable 48V converter, and thus the laser
         return
     """
     """
+    # Laser is powered-up and ARMED. While arming, status is 3. When ARMED, status is 4.
     def laser_arm(self):
         self.laser_commands.arm()
         status = self.get_status()
         self.oasis_serial(b'\x01')
         if self.states_laser <3:
-            self.states_laser = 3    #arming
+            self.states_laser = 3    # arming
         while status == 2:
             status = self.get_status()
         self.oasis_serial(b'\x20') # TODO: Fix from command.
@@ -36,18 +40,23 @@ class Laser():
         return
     """
     """
+    # Laser is powered-up and DISARMED
     def laser_disarm(self):
         self.laser_commands.disarm()
         return
     """
     """
+    # Laser must already be powered-up and ARMED to fire. When firing, status is 5.
     def laser_fire(self):
         self.laser_commands.fire_laser()
         return
     """
     """
+    # Laser is powered-off. When off, status is 0.
     def laser_off(self):
         # TODO
+
+        GPIO.output("P9_42", GPIO.LOW)  # set pin LOW to disable 48V converter, and thus the laser
         return
 
 
@@ -63,11 +72,12 @@ class Laser():
         # TODO: Some stuff
         output = 1
         return output
-
-
+    """
+    """
     def __init__(self, oasis_serial):
         self.oasis_serial = oasis_serial
         self.laser_commands = laser_control.Laser()
         # 0 = off, 1 = warming up, 2 = warmed up, 3 = arming, 4 = armed, 5 = firing, 6 = laser disconnected
         self.states_laser = 0
-        self.timer = time.time() #Only to initalize variable, not used.
+        self.timer = time.time()        # Only to initalize variable, not used.
+        GPIO.setup("P9_42", GPIO.OUT)   # 48V enable is on P9_42. ON = GPIO HIGH. OFF = GPIO LOW.
