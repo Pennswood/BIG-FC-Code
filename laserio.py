5cc7bb96bbfe502af12e833a89537aed46253c22
ksweens66 <ksweens66@gmail.com>
2020-06-05 00:26:48 -0400

    did this work?

--- laserio.py
+++ laserio.py
@@ -3,7 +3,7 @@ import time
 import Adafruit_BBIO.GPIO as GPIO       # Adafruit library for safe GPIO control
 from enum import Enum
 """
-Task: turn TLC to operations temperature, followed by turning on laser.
+Task:   turn TLC to operations temperature, followed by turning on laser.
         This function can be expected to be continuously called until laser is warmed up.
 Input: none
 Output: Integer. 0 = laser warmed up, 1 = laser warming up, 2 = TLC warming module up, >2 = some sort of error.
