436699063ce7c67e7a025930c6f1d8bf56372638
nschaffin <53974684+nschaffin@users.noreply.github.com>
2020-08-21 15:06:23 -0400

    Adding in various methods and threaded methods

--- spectrometerio.py
+++ spectrometerio.py
@@ -6,6 +6,7 @@ Manages communication with the spectrometer through the SeaBreeze library.
 import time
 import seabreeze
 import seabreeze.spectrometers
+import threading
 
 """
 IGNORE:		Spectrometer Error Bits (spec_error_bits)
@@ -18,8 +19,30 @@ spec_error_bits = [0, 1, 0]			# Spectrometer disconnected is a default state
 
 # TODO: Thread timer/wdt checking if spectrometer is still connected and if not set error bit to 1 (True)
 
+class SpecConnWDT(Exception):
+	pass
+
 class Spectrometer():
 	"""Class for interacting with the spectrometer through seabreeze."""
+
+	def __init__(self, serial, file_manager, spectrometer_state):
+		# 0 = standby, 1 = integrating, 2 = disconnected
+		self.oasis_serial = serial
+		# self.states_spectrometer = 0
+		self.fm = file_manager
+		self.devices = []
+		self.spec = self._setup_spectrometer()
+		self.spectrometer_state = spectrometer_state
+		self.reconnect_counter = 0
+		self._threads = []
+		self.wdt_stop = False
+
+		self.trigger_mode = 0
+		self.integration_time = 0
+		self.integration_time_left = 0
+		self.acquire_time_left = False
+		self.software_time_keeper_stop = False
+		self.read_delay = 0
 
 	def _setup_spectrometer(self):
 		"""
@@ -45,6 +68,71 @@ class Spectrometer():
 		print("ERROR: No spectrometer listed by seabreeze!")
 		return None
 
+	def _connection_wdt(self):
+		while True:
+			if self.wdt_stop == True:
+				break
+			if self.reconnect == True:
+				response = self.check_spec_conn()
+				self.reconnect_counter += 1
+				if self.reconnect_counter >= 5:
+					self.oasis_serial.sendBytes(b'\x00')	# TODO: Determine an error code for spectrometer having trouble connecting
+			else:
+				self.reconnect_counter = 0
+
+			if response = True:
+				self.reconnect_counter = 0
+				self.reconnect = False
+				self.oasis_serial.sendBytes(b'\x00')		# TODO: Determine a connection secured code to send to rover
+
+			time.sleep(1)
+
+	def _software_trigger_time_keeper(self):
+		self.spec.spectrum()								# Getting software trigger on track for integration cycle
+		while True:
+			start_time = time.time()
+			while time.time() - start_time < (self.integration_time + self.read_delay):
+				if self.acquire_time_left == True:
+					self.integration_time_left = self.integration_time - (time.time() - start_time) 			# TODO: How is this going to work?
+				if self.software_time_keeper_stop = True:
+					break
+			if self.software_time_keeper_stop = True:
+				break
+
+	def thread_controller(self, action):
+		"""
+		This is the control hub for all threads in the spectrometer class. Here you can start and terminate the WDT connection thread and software trigger time keeper thread.
+
+		Action inputs correspond to these commands: 0 = Start WDT Thread, 1 = Kill WDT Thread, 2 = Start Software Time Keeper Thread, 3 = Kill Software Time Keeper Thread
+		"""
+		if action == 0 and self.conn_wdt_thread not in self._threads:
+			self.wdt_stop = False														# This line will allow the thread to loop infinitely
+			self.conn_wdt_thread = threading.Thread(target=self._connection_wdt)
+			self.conn_wdt_thread.start()
+			self._threads.append(self.conn_wdt_thread)									# Append WDT thread to list, in case we need to close the thread
+
+		elif action == 1 and self.conn_wdt_thread in self._threads:
+			self.wdt_stop = True														# This line will cause the WDT thread to terminate
+			if self.conn_wdt_thread in self._threads:
+				self._threads.pop(self._threads.index(self.conn_wdt_thread))			# Removing the terminated thread from active threads list
+
+		elif action == 2 and self.software_timer_thread not in self._threads:
+			self.software_time_keeper_stop = False
+			self.software_timer_thread = threading.Thread(target=self._software_trigger_time_keeper)
+			self.software_timer_thread.start()
+			self._threads.append(self.conn_wdt_thread)
+
+		elif action == 3 and self.software_timer_thread in self._threads:
+			self.software_time_keeper_stop = True
+			if self.software_timer_thread in self._threads:
+				self._threads.pop(self._threads.index(self.software_timer_thread))
+
+		elif action != 0 or action != 1 or action != 2 or action != 3:
+			raise SpecConnWDT("Action command is not valid in connection WDT's state")
+
+		else:
+			raise SpecConnWDT("Action command is invalid")
+
 	# TODO: check if parts of this function should be refactored (i.e. saving errors into the sample data)
 	def sample(self, milliseconds):
 		"""
@@ -56,14 +144,11 @@ class Spectrometer():
 			Inputted integration time for spectrometer
 		"""
 		try:
-			# self.spec.trigger_mode = 0 									# Setting the trigger mode to normal
-			self.spec.integration_time_micros(milliseconds*1000) 		# Set integration time for spectrometer
 			self.spectrometer_state.integrate()
 			self.oasis_serial.sendBytes(b'\x01') 						# Sending nominal responce
-			wavelengths, intensities = self.spec.spectrum() 		# Returns wavelengths and intensities as a 2D array, and begins sampling
-			print('An error occurred while attempting to sample')  	# Command to sample didn't work properly
+			wavelengths, intensities = self.spec.spectrum() 			# Returns wavelengths and intensities as a 2D array, and begins sampling
+			print('An error occurred while attempting to sample')  		# Command to sample didn't work properly
 			data = wavelengths, intensities 							# Saving 2D array to variable data
-			# if data == []:
 			if intensities == [] or intensities == None:
 				print('No data entered')								# Error handling for no data collected
 			self.oasis_serial.sendBytes(b'\x30') 						# Code sent to spectrometer signaling sampling has successfully finished
@@ -72,9 +157,22 @@ class Spectrometer():
 			self.fm.save_sample(timestamp, data) 						# Function call to create spectrometer file
 			self.spectrometer_state.on_standby()
 		except:
-			self.check_spec_conn()
+			self.reconnect = True
 			return 'Checking spectrometer connection'
 		return None
+
+	def set_trigger(self, num=0):
+		self.spec.trigger_mode(num)
+
+	def set_integration_time_micros(self,time):
+		self.integration_time = milliseconds * 1000
+		self.spec.integration_time_micros(milliseconds*1000)
+		return self.integration_time
+
+	def _spectrometer_integration_boundries(self):
+		min_max = self.spec.integration_time_micros_limits
+		self.min_integration_time = min_max[0]
+		self.max_integration_time = min_max[1]
 		
 	def check_spec_conn(self):
 		"""
@@ -98,12 +196,3 @@ class Spectrometer():
 			is_open = self.devices[0].is_open
 			return is_open
 		return False
-
-	def __init__(self, serial, file_manager, spectrometer_state):
-		# 0 = standby, 1 = integrating, 2 = disconnected
-		self.oasis_serial = serial
-		# self.states_spectrometer = 0
-		self.fm = file_manager
-		self.devices = []
-		self.spec = self._setup_spectrometer()
-		self.spectrometer_state = spectrometer_state
